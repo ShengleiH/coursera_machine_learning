e213f9d0b8275dea41aa1d2649030e0b71f43262
Kelsey <shengleihuangchina@gmail.com>
2017-08-28 23:44:15 +0800

    submit

--- machine-learning-ex2/ex2-logistic regression/plotDecisionBoundary.m
+++ machine-learning-ex2/ex2-logistic regression/plotDecisionBoundary.m
@@ -13,18 +13,42 @@ plotData(X(:,2:3), y);
 hold on
 
 if size(X, 2) <= 3
-    % Only need 2 points to define a line, so choose two endpoints
-    plot_x = [min(X(:,2))-2,  max(X(:,2))+2];
+%     % Only need 2 points to define a line, so choose two endpoints
+%     plot_x = [min(X(:,2)),  max(X(:,2))]; % no matter which x1 you choose
+% %     plot_x = [X(5,2),  X(6,2)]; 
+% 
+%     % Calculate the decision boundary line
+%     plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1)); % (x1, x2) which is on X*theta==0
+% 
+%     % Plot, and adjust axes for better viewing
+%     plot(plot_x, plot_y)
+%     
+%     % Legend, specific for the exercise
+%     legend('Admitted', 'Not admitted', 'Decision Boundary')
+%     axis([30, 100, 30, 100])
 
-    % Calculate the decision boundary line
-    plot_y = (-1./theta(3)).*(theta(2).*plot_x + theta(1));
+    % Here is the grid range
+    u = linspace(30, 100, 100);
+    v = linspace(30, 100, 100);
 
-    % Plot, and adjust axes for better viewing
-    plot(plot_x, plot_y)
+    z = zeros(length(u), length(v));
+    % Evaluate z = theta*x over the grid
+    for i = 1:length(u)
+        for j = 1:length(v)
+            t = [1, u(i), v(j)];
+            z(i,j) = sigmoid(t * theta);
+        end
+    end
+    z = z'; % important to transpose z before calling contour
+    % Plot z = 0
+    % Notice you need to specify the range [0, 0]
+    contour(u, v, z, [0.5, 0.5], 'LineWidth', 2)
+    xlabel('Exam 1 score')
+    ylabel('Exam 2 score')
+    hold off;
     
-    % Legend, specific for the exercise
-    legend('Admitted', 'Not admitted', 'Decision Boundary')
-    axis([30, 100, 30, 100])
+    figure;
+    surf(u, v, z)
 else
     % Here is the grid range
     u = linspace(-1, 1.5, 50);
@@ -38,7 +62,6 @@ else
         end
     end
     z = z'; % important to transpose z before calling contour
-
     % Plot z = 0
     % Notice you need to specify the range [0, 0]
     contour(u, v, z, [0, 0], 'LineWidth', 2)
